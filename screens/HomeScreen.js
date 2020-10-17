5aa8b252dd237f4c4603cb9005571846f94a8521
prathmesh28 <prathmeshkerkar10@gmail.com>
2020-10-17 20:22:18 +0530

    form"

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react"  
 import Constants from 'expo-constants'
-
 import {
-    
   SafeAreaView,
   Dimensions,
   StatusBar,
@@ -11,11 +9,14 @@ import {
   TouchableOpacity,
   StyleSheet,
   FlatList,
-  View
+  View,
+  ImageBackground
 } from "react-native"  
 import { Container, Header, Content, Card, CardItem, Body, Text } from 'native-base';
 const { height, width } = Dimensions.get('screen')  
 import Firebase from '../firebase'
+import RNUrlPreview from 'react-native-url-preview';
+// import Bgimg from '../assets/Bgimg.svg'
 const DATA = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
@@ -51,13 +52,58 @@ export default class HomeScreen extends Component {
    <View style={styles.container}>
       <StatusBar hidden /> 
       <View>
+        {/* <ImageBackground source={Bgimg} width={width}> */}
+
+        
         <View style={{
+          
           //backgroundColor:'#7ec0ee',
           width:width,height:height*0.2,borderBottomWidth:1,borderBottomColor:'grey',
           justifyContent:"center",marginLeft:20}}>
         <Text style={{fontSize:20}}>
           Hello, {'\n'}<Text style={{fontSize:30,fontWeight:"bold"}}>Prathmesh!</Text>
         </Text>
+        <Text style={{fontSize:17,bottom:0,alignContent:"space-between",color:'#a9a9a9'}}>
+          How're you today?
+        </Text>
+        </View>
+        {/* </ImageBackground> */}
+        <View style={{width:width*0.9,alignSelf:"center",padding:10}}>
+        <Card style={{padding:0,borderRadius:20,elevation:4,borderColor:'#7ec0ee4d'}}>
+            <CardItem style={{padding:0,borderRadius:20, elevation:0,backgroundColor:'#7ec0ee66',borderColor:'#7ec0ee4d',borderWidth:1}}>
+              <Body>
+                <Text style={{fontSize:20,fontWeight:"900",alignSelf:'center'}}>
+                   Your next appointment is on
+                </Text>
+                <Text style={{fontSize:20,fontWeight:"bold",alignSelf:'center'}}>
+                  13 October 2020
+                </Text>
+              </Body>
+            </CardItem>
+          </Card>
+        </View>
+        <View style={{width:width*0.9,alignSelf:"center",padding:10}}>
+        <Card style={{padding:0,borderRadius:20,elevation:4,borderColor:'#7ec0ee4d'}}>
+            <CardItem 
+              style={{padding:0,borderRadius:20, elevation:0,alignContent:"center",
+              backgroundColor:'white',borderColor:'#7ec0ee4d',borderWidth:1}}>
+              <Body>
+                
+               
+         
+            <RNUrlPreview
+                text={'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public'} 
+                titleStyle={styles.linktitle}
+                titleNumberOfLines={3}
+              containerStyle={styles.linkcontainer}
+              imageStyle={styles.linkimage}
+              descriptionStyle={styles.discript}
+              descriptionNumberOfLines={4}
+            />
+               
+              </Body>
+            </CardItem>
+          </Card>
         </View>
         <View style={{alignItems:"center"}}>
         <FlatList
@@ -77,5 +123,27 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor:'#fff'
   },
+ 
+ 
+  linktitle: {
+  //  textAlign:"center",
+    fontWeight: "bold",
+    fontSize:17
+  },
+  linkcontainer: {
+    fontSize:15,
+    backgroundColor: "transparent",
+  },
+  linkimage: {
+    display: "none",
+  },
+  discript: {
+    
+    
+    padding:1
+    //dont remove
+  },
+  
+ 
 
 })  
