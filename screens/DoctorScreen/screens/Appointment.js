2c0138745733f8d96342fa3ef170305125927f61
jay <53287341+jay903@users.noreply.github.com>
2020-12-05 12:32:01 +0530

    data

--- screens/DoctorScreen/screens/Appointment.js
+++ screens/DoctorScreen/screens/Appointment.js
@@ -1,4 +1,4 @@
-import { Button, Card, CardItem } from "native-base"
+import { Button, Card, CardItem,} from "native-base"
 import React, { Component } from "react"
 
 import {
@@ -14,6 +14,8 @@ import {
 } from "react-native"
 const { height, width } = Dimensions.get('screen')
 import { Divider } from 'react-native-elements';
+ import Icon from 'react-native-vector-icons/Fontisto';
+ import Icon1 from 'react-native-vector-icons/Ionicons';
 
 export default class Appointment extends Component {
 
@@ -25,17 +27,20 @@ export default class Appointment extends Component {
           <View style={{alignSelf:"center"}}>
               <Text style={{fontSize:25}}>Appointment</Text>
           </View>
-        <Text style={{ fontSize: 20,marginTop:30 }}>Dr name </Text>
+        <Text style={{ fontSize: 20,marginTop:30 }}>{this.props.navigation.state.params.data.name} </Text>
         
         <Button  rounded bordered primary style={{width:100,marginTop:30}}>
+    <Text>{'\t'}</Text>
+          <Icon1 style={{ fontSize: 18 }} name="call-outline"/>
             <Text>  contact  </Text></Button>
         
         
       
-      <Card style={{width:width*0.5,alignSelf:"flex-start",borderRadius:15,marginTop:100}}>
+      <Card style={{width:width*0.4,alignSelf:"flex-start",borderRadius:15,marginTop:20}}>
         
      <CardItem  style={{borderRadius:15,backgroundColor:'#ffff',}}>
-            <Text>03-12-2020 | Tomorrow</Text>
+     <Icon style={{ fontSize: 18 }} name="date" />
+    <Text>{'\t'}{new Date().toLocaleDateString()}</Text>
             
             {/* <Divider style={{ backgroundColor: 'blue' }} />; */}
         </CardItem> 
@@ -43,13 +48,13 @@ export default class Appointment extends Component {
       </Card>
       
        
-     <Card style={{width:width*0.4,borderRadius:15,height:30}}>
+     {/* <Card style={{width:width*0.4,borderRadius:20,height:30}}>
                 <CardItem style={{borderRadius:15}}>
              <Text>select Date</Text>
              </CardItem>
              
          
-     </Card>
+     </Card> */}
       
      
 
@@ -64,9 +69,10 @@ export default class Appointment extends Component {
         padding: 5, width: width * .85, borderRadius: 10,height:height*0.2,marginTop:20
       }}>
           <CardItem>
-          <Text>10:00 AM - 5:00 PM </Text>
+          <Icon1 style={{ fontSize: 18 }} name="time-outline" />
+    <Text>{'\t'}{this.props.navigation.state.params.data.workFrom} - {this.props.navigation.state.params.data.workTo} </Text>
           </CardItem>
-          <Button rounded block info style={{bottom:-60}}>
+          <Button rounded block info style={{bottom:-30}}>
             <Text>JOIN QUEUE</Text>
             
           </Button>
